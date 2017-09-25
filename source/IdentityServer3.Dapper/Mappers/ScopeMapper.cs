4c0456a8d5c19020d2b50d387b573384efbec08c
Chao Guan <levid.gc@outlook.com>
2017-09-25 20:17:29 +0800

    add mapper for scope entity

---
+++ source/IdentityServer3.Dapper/Mappers/ScopeMapper.cs
@@ -0,0 +1,23 @@
+﻿using DapperExtensions.Mapper;
+using IdentityServer3.Dapper.Entities;
+
+namespace IdentityServer3.Dapper.Mappers
+{
+    public class ScopeMapper : ClassMapper<Scope>
+    {
+        public ScopeMapper()
+        {
+            // use a custom schema
+            Schema("dbo");
+
+            // use different table name
+            Table("Scopes");
+
+            // Ignore this property entirely
+            Map(x => x.ScopeClaims).Ignore();
+
+            // optional, map all other columns
+            AutoMap();
+        }
+    }
+}
