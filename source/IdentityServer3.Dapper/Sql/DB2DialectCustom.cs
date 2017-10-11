e5266fe493bd5fc205f17d65a6a14a222a410df9
Chao Guan <levid.gc@outlook.com>
2017-10-11 12:37:59 +0800

    add DB2Dialect custom

---
+++ source/IdentityServer3.Dapper/Sql/DB2DialectCustom.cs
@@ -0,0 +1,21 @@
+﻿using DapperExtensions.Sql;
+
+namespace IdentityServer3.Dapper.Sql
+{
+    public class DB2DialectCustom : DB2Dialect
+    {
+        // if we use the default '"' character for OpenQuote and CloseQuote, it can results in errors,
+        // because properties of our entities and column names of tables does not match exactly,
+        // therefore in our project, we left the OpenQuote and CloseQuote blank.
+
+        public override char OpenQuote
+        {
+            get { return ' '; }
+        }
+
+        public override char CloseQuote
+        {
+            get { return ' '; }
+        }
+    }
+}
